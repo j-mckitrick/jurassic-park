13ec9d3e16dbef36b2196c1c9162b16d67cc916d
Jonathon McKitrick <jmckitrick@gmail.com>
2015-12-07 19:30:42 -0500

    Show/update one cage

--- app/controllers/cages_controller.rb
+++ app/controllers/cages_controller.rb
@@ -1,4 +1,14 @@
 class CagesController < ApplicationController
+  before_filter :fetch_cage, :except => [:index, :create]
+
+  def fetch_cage
+    @cage = Cage.find_by_id(params[:id])
+  end
+
+  def show
+    render json: @cage
+  end
+
   def index
     render json: Cage.all
   end
@@ -12,6 +22,15 @@ class CagesController < ApplicationController
 
     if @cage.save
       render json: @cage, status: :created
+    else
+      render json: @cage.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    Rails.logger.info "Found cage to update #{@cage}"
+    if @cage.update_attributes(cage_params)
+      head :no_content, status: :ok
     else
       render json: @cage.errors, status: :unprocessable_entity
     end
