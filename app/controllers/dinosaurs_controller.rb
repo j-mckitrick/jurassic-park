61d5fc61f00a58fcbbd8c670415fdfefbeb67251
Jonathon McKitrick <jmckitrick@gmail.com>
2015-12-07 19:09:41 -0500

    Create a dinosaur via endpoint

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -2,4 +2,18 @@ class DinosaursController < ApplicationController
   def index
     render json: Dinosaur.all
   end
+
+  def dinosaur_params
+    params.permit(:name, :species, :classification, :cage)
+  end
+
+  def create
+    @dinosaur = Dinosaur.new(dinosaur_params)
+
+    if @dinosaur.save
+      render json: @dinosaur, status: :created
+    else
+      render json: @dinosaur, status: :unprocessable_entity
+    end
+  end
 end
