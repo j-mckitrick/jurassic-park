08d076492583c010bdabea48befdc5ec604f50a6
Jonathon McKitrick <jmckitrick@gmail.com>
2015-12-07 19:05:39 -0500

    Add endpoint to create a cage

--- app/controllers/cages_controller.rb
+++ app/controllers/cages_controller.rb
@@ -2,4 +2,18 @@ class CagesController < ApplicationController
   def index
     render json: Cage.all
   end
+
+  def cage_params
+    params.permit(:max_capacity, :current_count, :power_status)
+  end
+
+  def create
+    @cage = Cage.new(cage_params)
+
+    if @cage.save
+      render json: @cage, status: :created
+    else
+      render json: @cage, status: :unprocessable_entity
+    end
+  end
 end
