ab446d5a2ac847dd4162d0e6bd05f92a6c8d9d6b
Jonathon McKitrick <jmckitrick@gmail.com>
2015-12-07 19:45:11 -0500

    Add more validations

--- app/models/cage.rb
+++ app/models/cage.rb
@@ -1,3 +1,8 @@
 class Cage < ActiveRecord::Base
   validates :max_capacity, :current_count, :power_status, :presence => true
+  validates :max_capacity, :current_count, :numericality => { :only_integer => true }
+  validates :power_status, :inclusion => {
+              :in => %w(active down),
+              :message => "%{value} is not a valid power status"
+            }
 end
--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,4 +1,8 @@
 class Dinosaur < ActiveRecord::Base
   belongs_to :cage
   validates :name, :species, :classification, :presence => true
+  validates :classification, :inclusion => {
+              :in => %w(herbivore carnivore),
+              :message => "%{value} is not a valid classification"
+            }
 end
