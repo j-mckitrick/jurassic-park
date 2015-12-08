b2be15b784d6796f12937bfc0363afaccf9c68a4
Jonathon McKitrick <jmckitrick@gmail.com>
2015-12-07 20:09:33 -0500

    Add unit tests for cage

--- app/models/cage.rb
+++ app/models/cage.rb
@@ -1,8 +1,14 @@
 class Cage < ActiveRecord::Base
   validates :max_capacity, :current_count, :power_status, :presence => true
-  validates :max_capacity, :current_count, :numericality => { :only_integer => true }
-  validates :power_status, :inclusion => {
-              :in => %w(active down),
-              :message => "%{value} is not a valid power status"
-            }
+  validates :max_capacity, :current_count, :numericality => {:only_integer => true,
+                                                             :greater_than => 0}
+  validate :current_count_must_fit_within_max_capacity
+  validates :power_status, :inclusion => {:in => %w(active down),
+                                          :message => "%{value} is not a valid power status"}
+
+  def current_count_must_fit_within_max_capacity
+    if current_count > max_capacity
+      errors.add(:current_count, "Not enough room for this many dinosaurs")
+    end
+  end
 end
--- test/models/cage_test.rb
+++ test/models/cage_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  default_data = { :max_capacity => 10, :current_count => 0, :power_status => 'active' }
+  
+  test "cannot save cage without positive integer max capacity" do
+    cage = Cage.new(default_data)
+    cage.max_capacity = -1
+    assert_not cage.save, "Saved cage without a positive integer max capacity"
+  end
+
+  test "cannot save cage with current count greater than max capacity" do
+    cage = Cage.new(default_data)
+    cage.current_count = 20
+    assert_not cage.save, "Saved cage with current count greater than max capacity"
+  end
+
+  # This test should break without the validator, but it does not.  Find out why.
+  test "cannot save cage with invalid power status" do
+    cage = Cage.new(default_data)
+    cage.power_status = 'foo'
+    assert_not cage.save, "Saved cage with invalid power status"
+  end
 end
